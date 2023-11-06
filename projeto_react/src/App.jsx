5ffd47cad991a718270a3082b0a0269616be4cd1
Luis Eduardo <luiseduardf.a@gmail.com>
2023-11-06 13:38:51 -0300

    componentes

--- projeto_react/src/App.jsx
+++ projeto_react/src/App.jsx
@@ -1,9 +1,30 @@
+import MeuComponente from "./components/MeuComponetes"
 
 function App() {
 
   return (
-   <h1>Ola mundo, react</h1>
+   <div>
+
+     <h1>Ola mundo, react</h1>
+     <MeuComponente/>
+     <MeuBotao conteudo='me clique'/>
+
+     <MeuBotao conteudo='depois '/>
+
+     <MeuBotao conteudo='e por fim aqui'/>
+
+   </div>
   )
 }
+
+function MeuBotao(props){
+
+  console.log()
+  return(
+    <button>{props.conteudo}</button>
+  )
+}
+
+
 
 export default App
---
+++ projeto_react/src/components/MeuComponetes.jsx
@@ -0,0 +1,7 @@
+export default function MeuComponente() {
+    return (
+      <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit earum, eligendi aspernatur repudiandae blanditiis nobis recusandae amet soluta nihil ut possimus voluptatibus delectus a est dignissimos dolorem eveniet aut ipsam.</p>
+    )
+  }
+
+
